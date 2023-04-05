56a4aa07c76b78d0b94aff8b9144a75169da4331
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-04-05 20:41:21 +0300

    fixed task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-import UserCard from './UserCard/UserCard';
-import Statistics from './Statistics/Statistics';
 import data from '../json/data';
+import friends from '../json/friends';
 import transactions from '../json/transactions';
+import user from '../json/user';
+
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
     <>
-      <UserCard></UserCard>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList></FriendList>
+      <FriendList friends={friends}></FriendList>
       <TransactionHistory items={transactions}></TransactionHistory>
     </>
   );
--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,7 +1,7 @@
 import FriendListItem from '../FriendListItem/FriendListItem';
-import friends from '../../json/friends';
 
-const FriendList = () => {
+
+const FriendList = ({ friends }) => {
     return (
       <ul className="friend-list">
         {friends.map(({ id, ...friendProps }) => <FriendListItem key={id} {...friendProps}></FriendListItem>)}
